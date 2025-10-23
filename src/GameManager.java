09ecb014c14e4a5bb289a2aa4d3c71a114f764b4
Antonio Gabriel <antonio.gab003@gmail.com>
2025-10-23 20:32:31 +0100

    23/10

--- src/GameManager.java
+++ src/GameManager.java
@@ -4,36 +4,36 @@ import java.util.HashMap;
 
 public class GameManager {
     public boolean createInitialBoard(String[][] playerInfo, int worldSize){
-
+    return false;
     }
     public String getImagePng(int nrSquare){
-
+    return "";
     }
     public String[] getProgrammerInfo(int id){
-
+    return null;
     }
     public String getProgrammerInfoAsStr(int id){
-
+    return "";
     }
     public String[] getSlotInfo(int position){
-
+    return null;
     }
     public int getCurrentPlayerID(){
-
+    return 0;
     }
     public boolean moveCurrentPlayer(int nrSpaces){
-
+return false;
     }
     public boolean gameIsOver(){
-
+return false;
     }
     public ArrayList<String> getGameResults(){
-
+return null;
     }
     public JPanel getAuthorsPanel(){
-
+return null;
     }
     public HashMap<String, String> customizeBoard(){
-
+return null;
     }
 }
