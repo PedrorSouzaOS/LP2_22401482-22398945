1a6fc9c2a04ad69517725b2851a42fb8c3f118b5
PedrorSouza22401482 <a22401482@alunos.ulht.pt>
2025-10-23 20:01:11 +0100

    Initial commit test

---
+++ src/GameManager.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GameManager {
+    public boolean createInitialBoard(String[][] playerInfo, int worldSize){
+
+    }
+    public String getImagePng(int nrSquare){
+
+    }
+    public String[] getProgrammerInfo(int id){
+
+    }
+    public String getProgrammerInfoAsStr(int id){
+
+    }
+    public String[] getSlotInfo(int position){
+
+    }
+    public int getCurrentPlayerID(){
+
+    }
+    public boolean moveCurrentPlayer(int nrSpaces){
+
+    }
+    public boolean gameIsOver(){
+
+    }
+    public ArrayList<String> getGameResults(){
+
+    }
+    public JPanel getAuthorsPanel(){
+
+    }
+    public HashMap<String, String> customizeBoard(){
+
+    }
+}
