db709cb29ac7e2f6de441977d0bf15d0014803ee
PedrorSouza22401482 <a22401482@alunos.ulht.pt>
2025-10-24 14:35:35 +0100

    library

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -3,17 +3,49 @@ package pt.ulusofona.lp2.greatprogrammingjourney;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class GameManager {
-    GameManager(){}
+    public String[] tabuleiro;
+    public String[][] jogadores;
+    public GameManager(){
+        this.jogadores = new String[4][];
+        jogadores[0] = new String[]{"2", "Guido van Rossum", "Python", "Blue"};
+        jogadores[1] = new String[]{"3", "Ada Lovelace", "Ada", "Green"};
+        jogadores[2] = new String[]{"4", "John McCarthy", "Lisp", "Brown"};
+        jogadores[3] = new String[]{"1", "Joshua Bloch", "Java", "Purple"};
+    }
     public boolean createInitialBoard(String[][] playerInfo, int worldSize){
-    return false;
+        if (playerInfo.length!= 2 && playerInfo.length!=4){
+            return false;
+        }
+        String[] testeId = new String[playerInfo.length];
+    for(int i= 0; i<playerInfo.length; i++ ){
+        for(int j= 0;j<=i; j++){
+            if(Objects.equals(testeId[j], playerInfo[i][1])){
+                return false;
+            }
+            testeId[i] = playerInfo[i][1];
+        }
+    }
+
+        return true;
     }
     public String getImagePng(int nrSquare){
     return "";
     }
     public String[] getProgrammerInfo(int id){
-    return null;
+          String[][] var0 = new String[4][];
+            var0[0] = new String[]{"2", "Guido van Rossum", "Python", "Blue"};
+            var0[1] = new String[]{"3", "Ada Lovelace", "Ada", "Green"};
+            var0[2] = new String[]{"4", "John McCarthy", "Lisp", "Brown"};
+            var0[3] = new String[]{"1", "Joshua Bloch", "Java", "Purple"};
+        for (String[] strings : var0) {
+            if (Integer.parseInt(strings[1]) == id) {
+                return strings;
+            }
+        }
+        return null;
     }
     public String getProgrammerInfoAsStr(int id){
     return "";
@@ -37,6 +69,6 @@ public class GameManager {
         return null;
     }
     public HashMap<String, String> customizeBoard(){
-        return null;
+        return new HashMap<>();
     }
 }
